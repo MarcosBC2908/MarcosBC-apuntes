b2ea8082260b807c2aaba75df864b6787146f8d9
marcosbc@msmk.university <marcosbc@msmk.university>
2024-04-10 13:38:14 +0200

    apuntes components marcosbc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './shared/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'MarcosBC-apuntes';
+
+
+operadores: string [] = ["+", "-", "*", "/", "&&", "||", "===", "!=="];
+
+
+
+
+
+
+
+
+
 }
